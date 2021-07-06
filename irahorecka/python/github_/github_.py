3dcee088d3773669533a8f1d07ef568581e49f26
Ira Horecka <ira89@icloud.com>
2021-07-05 21:30:31 -0700

    add get_lang attribute in github_

--- irahorecka/python/github_/github_.py
+++ irahorecka/python/github_/github_.py
@@ -27,7 +27,7 @@ JSON_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "out.json")
 
 def write_repos(access_token):
     """Entry point function to write GitHub user's repos (via access token)
-    to `JSON_PATH`."""
+    to `JSON_PATH`. Returns exit code 0 if success, 1 if failure."""
     try:
         user = Github(access_token).get_user()
     except RateLimitExceededException:
@@ -40,7 +40,8 @@ def write_repos(access_token):
 
 
 def build_repos_json(repos):
-    return {key: value for repo in map_threads(build_repo_json, repos) for key, value in repo.items()}
+    """Get projects' name and information as a dictionary."""
+    return {proj: info for repo in map_threads(build_repo_json, repos) for proj, info in repo.items()}
 
 
 def map_threads(func, _iterable):
@@ -62,6 +63,7 @@ def build_repo_json(repo):
             "forks": repo.forks_count,
             "commits": len(list(validate_gh_method(repo.get_commits))),
             "open_issues": repo.open_issues_count,
+            "languages": validate_gh_method(repo.get_languages),
         }
     }
 
