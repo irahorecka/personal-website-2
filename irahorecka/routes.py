209b78979d452e5022aee5577bf97c1d4295f7e7
Ira Horecka <ira89@icloud.com>
2021-07-06 23:49:15 -0700

    update photos and content

--- irahorecka/routes.py
+++ irahorecka/routes.py
@@ -18,8 +18,8 @@ def home():
     content = {
         "title": "Ira Horecka | Home",
         "profile_img": "profile.png",
-        "header": get_header("home"),  # Use Jinja notation of content.header.first, content.header.second, etc.
-        "body": get_body("home"),  # Use Jinja notation of content.body.first, content.body.second, etc.
+        "header": get_header("home"),  # Use Jinja syntax s/a `content.header.first`, `content.header.second`, etc.
+        "body": get_body("home"),  # Use Jinja syntax s/a `content.body.first`, `content.body.second`, etc.
     }
     return render_template(
         "home.html",
@@ -32,12 +32,12 @@ def api():
     """API page of personal website."""
     content = {
         "title": "Ira Horecka | API",
-        "profile_img": "profile.png",
+        "profile_img": "me_arrow.png",
         "header": get_header("api"),
         "body": get_body("api"),
     }
     return render_template(
-        "home.html",
+        "layout.html",
         content=content,
     )
 
@@ -47,7 +47,7 @@ def projects():
     """GitHub projects page of personal website."""
     content = {
         "title": "Ira Horecka | API",
-        "profile_img": "me_arrow.png",
+        "profile_img": "me_computing.png",
         "header": get_header("projects"),
         "body": get_body("projects"),
         "repos": read_repos(),
