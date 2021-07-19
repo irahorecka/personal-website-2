33ad4d944be06f747345cda6e99a62d77004b270
Ira Horecka <ira89@icloud.com>
2021-07-18 17:47:27 -0700

    update utility scripts for db and db clean

--- script.py
+++ script.py
@@ -5,12 +5,11 @@ from irahorecka.models import db, CraigslistHousing
 from irahorecka.python.api.craigslisthousing.utils import read_neighborhoods, write_neighborhoods
 from irahorecka.python.api.craigslisthousing.update import (
     clean_craigslist_housing,
-    calculate_post_score,
-    write_price_per_area,
+    write_craigslist_housing_score,
 )
 
 if __name__ == "__main__":
     # print(read_neighborhoods())
-    write_neighborhoods(db.session, CraigslistHousing)
+    # write_neighborhoods(db.session, CraigslistHousing)
     clean_craigslist_housing()
-    write_price_per_area()
+    write_craigslist_housing_score("sfbay", ["eby", "nby", "sby", "sfc", "pen", "scz"])
--- setup_db.py
+++ setup_db.py
@@ -1,12 +1,12 @@
 import os
 
-from irahorecka.models import db, CraigslistHousing
+from irahorecka.models import db
 import irahorecka.python.content as ct
 import irahorecka.python.api as api
 
 db.create_all()
 
-# ct.write_github_repos(os.environ.get("GITHUB_TOKEN"))
+ct.write_github_repos(os.environ.get("GITHUB_TOKEN"))
 api.write_craigslist_housing(site="sfbay", areas=["eby", "nby", "sby", "sfc", "pen", "scz"])
-# api.clean_craigslist_housing()
-# api.write_neighborhoods(db.session, CraigslistHousing)
+api.clean_craigslist_housing()
+api.write_craigslist_housing_score(site="sfbay", areas=["eby", "nby", "sby", "sfc", "pen", "scz"])
