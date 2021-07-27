3079ac652f83de9d513ed117346a5664c9a5181d
Ira Horecka <ira89@icloud.com>
2021-07-27 01:09:35 -0700

    add error pages

--- irahorecka/errors/handlers.py
+++ irahorecka/errors/handlers.py
@@ -5,6 +5,7 @@ errors = Blueprint("errors", __name__)
 
 @errors.app_errorhandler(404)
 def error_404(error):
+    """Error: Page not found"""
     content = {
         "title": "Error 404",
         "profile_img": "feynman.jpeg",
@@ -14,6 +15,7 @@ def error_404(error):
 
 @errors.app_errorhandler(403)
 def error_403(error):
+    """Error: Forbidden"""
     content = {
         "title": "Error 403",
         "profile_img": "trevor.jpeg",
@@ -23,6 +25,7 @@ def error_403(error):
 
 @errors.app_errorhandler(500)
 def error_500(error):
+    """Error: Internal Server Error"""
     content = {
         "title": "Error 500",
         "profile_img": "cory.jpeg",
