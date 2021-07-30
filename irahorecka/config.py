cb39b77d3f3a326766a27f7c9bae89fa4f8c90fb
Ira Horecka <ira89@icloud.com>
2021-07-29 17:19:13 -0700

    update setup_db

--- irahorecka/config.py
+++ irahorecka/config.py
@@ -3,12 +3,16 @@
 
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 class Config:
     """Flask app configuration class."""
 
     DEBUG = True
     TESTING = False
-    SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    SECRET_KEY = os.environ["SECRET_KEY"]
+    SQLALCHEMY_DATABASE_URI = os.environ["SQLALCHEMY_DATABASE_URI"]
     SQLALCHEMY_TRACK_MODIFICATIONS = False
