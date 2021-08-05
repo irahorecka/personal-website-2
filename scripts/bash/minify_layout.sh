e5a7ab538017c29f1284f6e51fea787ca9f0a0dc
Ira Horecka <ira89@icloud.com>
2021-08-05 09:53:28 -0700

    update minify_layout.sh

--- scripts/bash/minify_layout.sh
+++ scripts/bash/minify_layout.sh
@@ -2,6 +2,7 @@
 
 # Convert ./irahorecka/templates/layout.html to a minified version
 # Store output as ./prod/irahorecka/templates/layout.html
+mkdir ./prod/irahorecka/templates;
 cp ./irahorecka/templates/layout.html ./prod/irahorecka/templates/layout.html;
 perl -i -p0e 's/{% assets.*?assets %}/<link rel="stylesheet" href="{{ url_for('\''static'\'', filename='\''css\/dist\/main.min.css'\'') }}">/s' ./prod/irahorecka/templates/layout.html;
 sed -i '' '/.min.js/! s/\([^.]*\)\.js/\1.min.js/g' ./prod/irahorecka/templates/layout.html;
